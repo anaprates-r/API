a6a42909a6c0df1716cbf1a8cd8c9bd58202e85b
anaprates-r <ana.prates@ufms.br>
2025-06-08 19:09:18 -0400

    Create kdtree_wrapper.py

---
+++ api/kdtree_wrapper.py
@@ -0,0 +1,32 @@
+import ctypes
+from ctypes import Structure, POINTER, c_double, c_int,c_char,c_float
+
+class TReg(Structure):
+    _fields_ = [("emb", c_float *128), 
+                ("id",c_char *100)
+                ]
+
+class TNode(Structure):
+    pass
+
+TNode._fields_ = [("key", ctypes.c_void_p),
+                  ("esq", POINTER(TNode)),
+                  ("dir", POINTER(TNode))]
+
+class Tarv(Structure):
+    _fields_ = [("k", c_int),
+                ("dist", ctypes.CFUNCTYPE(c_double, ctypes.c_void_p, ctypes.c_void_p)),
+                ("cmp", ctypes.CFUNCTYPE(c_double, ctypes.c_void_p, ctypes.c_void_p, c_int)),
+                ("raiz", POINTER(TNode))]
+
+# Carregar a biblioteca C
+lib = ctypes.CDLL("./kdtree.dll")
+
+# Definir a assinatura da função
+lib.buscar_mais_proximo.argtypes = [POINTER(Tarv), TReg]
+lib.buscar_mais_proximo.restype = TReg
+lib.get_tree.restype = POINTER(Tarv)
+lib.inserir_ponto.argtypes = [TReg]
+lib.inserir_ponto.restype = None
+lib.kdtree_construir.argtypes = []
+lib.kdtree_construir.restype = None
